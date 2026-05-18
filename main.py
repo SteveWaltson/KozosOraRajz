500bf6b1146f1bca884fb95dc8225d6f60dc6e0b
BalajtiDani <Gyertya666@gmail.com>
2026-05-18 09:38:21 +0200

    Középső mutató (kék)

---
+++ main.py
@@ -0,0 +1,12 @@
+#Sanyi Másodperc
+#balazs ora
+#Dani perc
+import turtle
+
+turtle.color("blue")
+turtle.goto(0, 0)
+turtle.fillcolor("blue")
+turtle.pensize(10)
+turtle.right(3)
+turtle.forward(200)
+turtle.done()
